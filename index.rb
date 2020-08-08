8201b34ffad54a10aaa964408c1c12f96e499bb2
TJBachorz <TJBachorz@gmail.com>
2020-08-08 10:57:44 -0600

    8/8/20 codewars

--- index.rb
+++ index.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 #CODEWARS CHALLENGES
 
 #-----------------------
@@ -5,6 +7,21 @@
 
 def opposite_num num
     num * (-1)
+end
+
+#-----------------------
+# 8/8/20
+
+def series_sum(n)
+    return "0.00" if n == 0
+    numerator = 1
+    denom = 1
+    total = 0.to_f
+    n.times do
+      total += (numerator / denom.to_f).round(2)
+      denom += 3
+    end
+    total.to_s
 end
 
 #-----------------------
