1a95bc7c62ee87ae0134f714833b6d21350f28c5
TJBachorz <TJBachorz@gmail.com>
2020-08-24 09:05:03 -0600

    Codewars 8/24/20

--- index.py
+++ index.py
@@ -6,11 +6,20 @@ import pdb
 def reverse(string):
    return string[::-1]
 
-
-test = "hello"
 #--------------------------------------------
-# 8/17/20
+# 8/24/20
+
+def get_sum(a,b):
+    if a < b:
+        return sum(range(a, b+1, 1))
+    else:
+        return sum(range(b, a+1, 1))
+
+#--------------------------------------------
+
+
+
+
+
 
 pdb.set_trace()
-
-#--------------------------------------------
