20e5926362c7534792d2e8f3e9d4af8e52589724
TJBachorz <TJBachorz@gmail.com>
2020-10-15 09:04:39 -0600

    Codewars 10/15/20

--- index.js
+++ index.js
@@ -519,3 +519,27 @@ function heavyPill(pills) {
         toBeWeighed.push(pills.slice(0, i+1))
     }
 }
+
+//---------------------------------------------
+// 10/15/20
+function getPINs(observed) {
+    const checks = {
+        1: [1,2,4],
+        2: [1,2,4,5],
+        3: [2,3,6],
+        4: [1,4,5,7],
+        5: [2,4,5,6,8],
+        6: [3,5,6,9],
+        7: [4,7,8],
+        8: [5,7,8,9,0],
+        9: [6,8,9],
+        0: [8,0] 
+    }
+    
+    let splitNum = observed.split("")
+    let combos = []
+    splitNum.forEach(num)
+  //   for (let i=0; i < splitNum.length; i++) {
+  //     splitNum[i]
+    }
+}
