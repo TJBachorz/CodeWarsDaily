f9cc131073fa629adfb7a38fa8c3bfa3e39addb7
TJBachorz <TJBachorz@gmail.com>
2021-01-05 17:19:19 -0700

    Codewars 1/5/21

--- Codewars2021/index.py
+++ Codewars2021/index.py
@@ -5,3 +5,12 @@ def sum_digits(number):
     return sum(int(i) for i in str(abs(number)))
 
 #------------------------------------
+# 1/5/21
+
+def capitals(word):
+    return [i for i in range(len(word)) if word[i].isupper()]
+
+def divisors(n):
+    return len([num for num in range(1, n+1) if n % num == 0])
+
+#-------------------------------------
